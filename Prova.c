fbd38777992246ef4c8b03bdd0f0f26170f3ca66
Fernando Alves <139921504+fernandoalvess@users.noreply.github.com>
2023-07-31 15:39:18 -0300

    Add files via upload

---
+++ Prova.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Professor {
+
+    char nome [100];
+    char discip [100];
+    float horasTrab;
+
+}Professor;
+
+typedef struct Professor;
+{
+    
+int main (void) {
+
+    printf("Digite o nome:\n");
+    scanf("%[^\n]s", professor.nome);
+
+    printf("Digite a disciplina:\n");
+    scanf("%[^\n]s", professor.discip );
+
+    printf("Informe o regime de trabalho:\n");
+    scanf("%f", &horasTrab);
+    }
+};
+
+
