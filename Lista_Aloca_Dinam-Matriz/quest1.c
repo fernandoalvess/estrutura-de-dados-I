512a87822a7f9a0d7841354035de0ad124bbff0f
fernandoalvess <139921504+fernandoalvess@users.noreply.github.com>
2023-07-31 11:44:31 -0300

    Add files via upload

---
+++ Lista_Aloca_Dinam-Matriz/quest1.c
@@ -0,0 +1,34 @@
+/*Alocar uma matyriz 5x5 e preencher cada elemento,
+depois imprimir a metriz*/
+
+#include <stdio.h>
+#include <stdlib.h>
+ int main () {
+
+    int linha = 5, coluna =5, i, j; //'i' para linha e 'j' para coluna
+
+    int **matriz = (int **) malloc (linha * sizeof(int *)); 
+
+    for (i=0; i<linha; i++) {
+        matriz[i] = (int *) malloc (coluna * sizeof(int));
+    }
+
+    printf("Informe os valores da Matriz\n");
+    for(i=0; i<5; i++) { //Pode ser substituido o numero '5' pela variavel 'linha'
+        for(j=0; j<5; j++){ ////Pode ser substituido o numero '5' pela variavel 'coluna'
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("\n\nImprimindo toda a Matriz\n");
+    for(i=0; i<5; i++) {
+        for(j=0; j<5; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+    printf("\n");
+    }
+     
+    free(matriz);
+
+    return 0;
+ }
